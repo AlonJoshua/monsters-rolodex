efef8c19dfe7b14a3baed58ea1cdffcee6cc4dd7
AlonJoshua <alon@boaideas.com>
2023-09-23 23:51:34 +0300

    fetching users, filtering searchbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
   constructor() {
     super();
-    
+
     this.state = {
-      name: 'Alon'
-    }
+      monsters: [],
+      filteredMonsters: [],
+      searchQuery: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState({ monsters: users, filteredMonsters: users })
+      );
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchQuery);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Change name</button>
-        </header>
+        <input
+          type="search"
+          className="search-box"
+          onInput={(event) => {
+            const searchQuery = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchQuery };
+            });
+          }}
+          placeholder="Search"
+        ></input>
+        {filteredMonsters.map((monster) => (
+          <div key={monster.name}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))}
       </div>
     );
   }
